0c4ccdefb9a0bbb1d03b09858bbca45882193176
Kyle van Niekerk <kylevn185@gmail.com>
2024-09-23 14:22:39 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NodeOPCUA-Project
+A JavaScript project to follow along with the book NodeOPCUA By Example
---
+++ my_test_program.ts
@@ -0,0 +1,7 @@
+(async ()=> {
+    try {
+        console.log("Hello World");
+    } catch (err) {
+        console.log("Error: ", err)
+    }
+})();
