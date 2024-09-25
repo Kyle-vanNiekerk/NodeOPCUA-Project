119a1fcf00d13c4cf65e9516420b89b09427ca15
Kyle van Niekerk <kylevn185@gmail.com>
2024-09-25 13:49:29 +0200

    Setup prerequisites, config, debugger

---
+++ build/my_test_program.js
@@ -0,0 +1,11 @@
+"use strict";
+/* September 2024 */
+(async () => {
+    try {
+        console.log("Hello World");
+    }
+    catch (err) {
+        console.log("Error: ", err);
+    }
+})();
+//# sourceMappingURL=my_test_program.js.map
--- my_test_program.ts
+++ my_test_program.ts
@@ -3,10 +3,7 @@
 (async ()=> {
     try {
         console.log("Hello World");
-        console.log("Justin");
-        console.log("Thendo");
     } catch (err) {
         console.log("Error: ", err)
-
     }
 })();
